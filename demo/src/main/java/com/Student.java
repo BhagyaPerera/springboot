f4ce58f1f4b0dc06f1258dacf69cc1073e046bb0
Bhagya Perera <wbmadushani@gmail.com>
2020-08-02 21:45:54 +0530

    data model done

---
+++ demo/src/main/java/com/Student.java
@@ -0,0 +1,69 @@
+package com;
+
+public class Student {
+
+        private int id;
+        private String fName;
+        private String lname;
+
+
+public Student() {
+    
+}
+
+public Student(fname,lname) {
+
+    this.fname=fname;
+    this.lname=lname;
+    
+}
+
+
+
+    
+
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the fName
+     */
+    public String getFName() {
+        return fName;
+    }
+
+    /**
+     * @param fName the fName to set
+     */
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    /**
+     * @return String return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    
+
+}
